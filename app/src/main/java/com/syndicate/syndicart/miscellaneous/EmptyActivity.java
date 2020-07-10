e49492bababcfb83c4f5d1165aeaaf43a9374af6
akash-mani <akashrocks352@gmail.com>
2020-07-10 19:28:01 +0530

    Initial commit

---
+++ app/src/main/java/com/syndicate/syndicart/miscellaneous/EmptyActivity.java
@@ -0,0 +1,14 @@
+package com.syndicate.syndicart.miscellaneous;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.syndicate.syndicart.R;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
